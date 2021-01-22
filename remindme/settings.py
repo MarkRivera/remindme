14c59166b8788ab5cc726c422ff488186ca7ef66
Mark Rivera <Mriver1991@gmail.com>
2021-01-22 17:40:01 -0500

    moved debug var to env

--- remindme/settings.py
+++ remindme/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['remindmedjango.herokuapp.com']
 
 
 # Application definition
